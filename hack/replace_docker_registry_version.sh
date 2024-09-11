2a89758665a249a183932ae9d5e7af480c700b0e
Cortey <andrzej.pankowski@sap.com>
2024-09-11 11:36:30 +0200

    automate_docker_registry_version_bump

---
+++ hack/replace_docker_registry_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KUSTOMIZATION_DIRECTORY=${PROJECT_ROOT}/config/operator/base
+KUSTOMIZATION_FILE=${KUSTOMIZATION_DIRECTORY}/kustomization.yaml
+
+REQUIRED_ENV_VARIABLES=('IMG_VERSION' 'PROJECT_ROOT')
+for VAR in "${REQUIRED_ENV_VARIABLES[@]}"; do
+  if [ -z "${!VAR}" ]; then
+    echo "${VAR} is undefined"
+    exit 1
+  fi
+done
+
+VERSION_SELECTOR='.commonLabels."app.kubernetes.io/version"'
+yq --inplace "${VERSION_SELECTOR} = \"${IMG_VERSION}\"" ${KUSTOMIZATION_FILE}
