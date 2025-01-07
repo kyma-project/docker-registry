6f235f47824664f98eefacad31beaaa4fbd69f88
pPrecel <filip.strozik@outlook.com>
2025-01-07 10:15:37 +0100

    improve rollme flag building

--- components/operator/internal/chart/flags_test.go
+++ components/operator/internal/chart/flags_test.go
@@ -47,3 +47,33 @@ func Test_flagsBuilder_Build(t *testing.T) {
 		require.Equal(t, expectedFlags, flags)
 	})
 }
+
+func Test_flagsBuilder_withRollme(t *testing.T) {
+	t.Run("add rollme flag", func(t *testing.T) {
+		flags := flagsBuilder{
+			flags: map[string]interface{}{},
+		}
+
+		_ = flags.withRollme("reason=test")
+
+		expectedFlags := map[string]interface{}{
+			"rollme": "reason=test",
+		}
+		require.Equal(t, expectedFlags, flags.Build())
+	})
+
+	t.Run("add value to existing rollme flag", func(t *testing.T) {
+		flags := flagsBuilder{
+			flags: map[string]interface{}{
+				"rollme": "reason=test",
+			},
+		}
+
+		_ = flags.withRollme("another-reason=test-2")
+
+		expectedFlags := map[string]interface{}{
+			"rollme": "reason=test,another-reason=test-2",
+		}
+		require.Equal(t, expectedFlags, flags.Build())
+	})
+}
