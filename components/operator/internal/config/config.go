ae2da8ef4d95b9b26d04f1671d5121fe49b249d6
Cortey <andrzej.pankowski@sap.com>
2025-12-08 14:51:16 +0100

    logging

--- components/operator/internal/config/config.go
+++ components/operator/internal/config/config.go
@@ -1,14 +1,86 @@
 package config
 
-import "github.com/vrischmann/envconfig"
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/vrischmann/envconfig"
+	"gopkg.in/yaml.v3"
+)
 
 type Config struct {
-	ChartPath string `envconfig:"default=/module-chart"`
+	ChartPath string `envconfig:"default=/module-chart" yaml:"chartPath"`
+	LogLevel  string `envconfig:"default=info" yaml:"logLevel"`
+	LogFormat string `envconfig:"default=json" yaml:"logFormat"`
 }
 
 func GetConfig(prefix string) (Config, error) {
 	cfg := Config{}
 	err := envconfig.InitWithPrefix(&cfg, prefix)
 	return cfg, err
+}
 
+func readConfigFile(cfgPath string) (Config, error) {
+	cfg := Config{}
+	data, err := os.ReadFile(cfgPath)
+	if err != nil {
+		return cfg, err
+	}
+	err = yaml.Unmarshal(data, &cfg)
+	return cfg, err
+}
+
+// RunOnConfigChange watches for config changes and executes the callback function
+// When Kubernetes updates ConfigMaps, it atomically updates symlinks, so we watch the parent directory
+func RunOnConfigChange(ctx context.Context, log interface{ Info(...interface{}) }, cfgPath string, onChangeFunc func(Config)) {
+	if cfgPath == "" {
+		return
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Info("unable to create config watcher", "error", err)
+		return
+	}
+	defer func() {
+		if err := watcher.Close(); err != nil {
+			log.Info("error closing watcher", "error", err)
+		}
+	}()
+
+	// Watch the directory containing the config file to catch Kubernetes ConfigMap updates
+	// which are done via atomic symlink changes
+	configDir := filepath.Dir(cfgPath)
+
+	if err := watcher.Add(configDir); err != nil {
+		log.Info("unable to watch config directory", "error", err)
+		return
+	}
+
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case event := <-watcher.Events:
+			// Kubernetes ConfigMap updates trigger Create events on the ..data symlink
+			if event.Op&fsnotify.Create == fsnotify.Create || event.Op&fsnotify.Write == fsnotify.Write {
+				cfg, err := readConfigFile(cfgPath)
+				if err != nil {
+					log.Info("unable to read config", "error", err)
+					continue
+				}
+				onChangeFunc(cfg)
+			}
+		case err := <-watcher.Errors:
+			log.Info("config watcher error", "error", err)
+		case <-ticker.C:
+			// Periodic check to ensure watcher is still active
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ toolchain go1.24.3
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/go-logr/zapr v1.3.0
 	github.com/google/uuid v1.6.0
+	github.com/kyma-project/manager-toolkit v0.0.0-20251201082204-449f6914b121
 	github.com/onsi/ginkgo/v2 v2.27.2
 	github.com/onsi/gomega v1.38.2
 	github.com/pkg/errors v0.9.1
@@ -22,6 +25,7 @@ require (
 	k8s.io/apiextensions-apiserver v0.33.6
 	k8s.io/apimachinery v0.33.6
 	k8s.io/client-go v0.33.6
+	k8s.io/klog/v2 v2.130.1
 	k8s.io/utils v0.0.0-20241104163129-6fe5fd82f078
 	sigs.k8s.io/controller-runtime v0.20.4
 )
@@ -51,12 +55,10 @@ require (
 	github.com/evanphx/json-patch/v5 v5.9.11 // indirect
 	github.com/exponent-io/jsonpath v0.0.0-20210407135951-1de76d718b3f // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-gorp/gorp/v3 v3.1.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
-	github.com/go-logr/zapr v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
@@ -138,7 +140,6 @@ require (
 	k8s.io/apiserver v0.33.6 // indirect
 	k8s.io/cli-runtime v0.33.3 // indirect
 	k8s.io/component-base v0.33.6 // indirect
-	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff // indirect
 	k8s.io/kubectl v0.33.3 // indirect
 	oras.land/oras-go/v2 v2.6.0 // indirect
