666b17ab27bae080395b91ac6503e93c182c7c06
Piotr Halama <piotr.halama@sap.com>
2026-06-08 08:30:32 +0200

    Add config for main branch security scans (#555)

--- .github/scripts/upgrade-component-config.sh
+++ .github/scripts/upgrade-component-config.sh
@@ -3,7 +3,7 @@
 IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
 yq eval-all --inplace '
-    select(fileIndex == 0).bdba=[
+    select(fileIndex == 0).images=[
         select(fileIndex == 1)
         | .containerRegistry.path as $registryPath
         | (
@@ -18,4 +18,4 @@ yq eval-all --inplace '
         | $registryPath + "/" + .directory + "/" + .name + ":" + .version
     ]
     | select(fileIndex == 0)
-    ' sec-scanners-config.yaml config/docker-registry/values.yaml
+    ' component-config.yaml config/docker-registry/values.yaml
